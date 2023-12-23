cf18b6ce7c06a8a29102719e5a06634b44e90b1b
manug <manug@Manoj_Kumar_M.mshome.net>
2023-12-23 18:24:16 +0530

    Manoj Push

---
+++ ShoppersStock/src/test/java/shopperstock/logintest/Manoj.java
@@ -0,0 +1,13 @@
+package shopperstock.logintest;
+
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class Manoj {
+
+	@Test
+	public void manoj() {
+		Reporter.log("Manoj", true);
+	}
+	
+}
